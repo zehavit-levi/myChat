f1937fbf71b7a5171cee88cc95587be2c0cf6826
zehavit-levi <levi.zehavit@gmail.com>
2021-10-14 13:32:41 +0300

    remove dupliacte lines

--- server/index.js
+++ server/index.js
@@ -10,6 +10,3 @@ app.get("/api", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
-});
