c3bc2e8bd880af79959ff49ae88511429626798b
zehavit-levi <levi.zehavit@gmail.com>
2021-10-17 13:25:07 +0300

    add react router dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,75 @@
-
-import React from 'react'
+import React from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+  useLocation
+} from "react-router-dom";
+import Login from './pages/Login/Login';
+import RoomList from './pages/RoomList/RoomList';
+import AddRoom from './pages/AddRoom/AddRoom';
+import ChatRoom from './pages/ChatRoom/ChatRoom';
 
 
 function App() {
 
  
-  const [data, setData] = React.useState(null);
+  // const [data, setData] = React.useState(null);
 
-  React.useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
-  }, []);
-
+  // React.useEffect(() => {
+  //   fetch("/api")
+  //     .then((res) => res.json())
+  //     .then((data) => setData(data.message));
+  // }, []);
+  
+  let location = useLocation();
+  
+  function SecureRoute({ children, ...rest }) {
+    return (
+      <Route
+        {...rest}
+        render={({ location }) =>
+          localStorage.getItem('nickname') ? (
+            children
+          ) : (
+            <Redirect
+              to={{
+                pathname: "/login",
+                state: { from: location }
+              }}
+            />
+          )
+        }
+      />
+    );
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>{!data ? "Loading..." : data}</p>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Redirect
+          to={{
+            pathname: "/roomlist",
+            state: { from: location }
+          }}
+        />
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <SecureRoute path="/roomlist">
+            <RoomList />
+          </SecureRoute>
+          <SecureRoute path="/addroom">
+            <AddRoom />
+          </SecureRoute>
+          <SecureRoute path="/chatroom/:room">
+            <ChatRoom />
+          </SecureRoute>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
