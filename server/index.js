7d98f26ca020341edd780788a94c3d811ffe7125
zehavit-levi <levi.zehavit@gmail.com>
2021-10-17 19:43:55 +0300

    add first decleration of socket server

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 
 const express = require("express");
+const socket = require("socket.io");
 const app = express();
-
 const cors = require("cors");
+
 app.use(cors());
+app.use(express.json);
 
 const PORT = process.env.PORT || 3001;
 
@@ -12,6 +14,24 @@ app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
+
+io = socket(server);
+io.on('connection',socket =>{
+  console.log(socket.id)
+
+  socket.on('join_room', (data) =>{
+    socket.join(data);
+    console.log('User Joined Room: ' + data)
+  })
+
+
+
+
+  socket.on('disconnect',()=>{
+    console.log("USER DISCONNECTED")
+  })
+})
+
