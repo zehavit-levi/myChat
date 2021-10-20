d85620622139b2c00f5bc840631147009336b2e6
zehavit-levi <levi.zehavit@gmail.com>
2021-10-20 19:43:19 +0300

    add bubble like view + scroll chat message fixed

--- client/src/pages/ChatRoom/ChatRoom.jsx
+++ client/src/pages/ChatRoom/ChatRoom.jsx
@@ -1,41 +1,46 @@
-import { Container, Row ,Col} from 'react-bootstrap';
+import { Container, Row ,Col, Button} from 'react-bootstrap';
+import { Scrollbars } from 'react-custom-scrollbars';
+import Picker from 'emoji-picker-react';
 import './ChatRoom.css'
+import { useState } from 'react';
 function ChatRoom(props){
+ 
     return(
         <Container className='chatRoomContainer'>
-            <Row>
-                <Col className="chatUsersList">
+            <Row className="usersandchatrow">
+                <Col sm='3' className="chatUsersList">
                     {props.usersList.map((val,key)=>{
                         return(
                          <div>{val}</div>
                       )
                   })}
                 </Col>
-                <Col className="chatContainer">
-                    <Row className="messages">
+                <Col sm='9' className="chatContainer">
+                    <Scrollbars ><Row className="messages">
                     {props.messageList.map((val,key)=>{
                     return (
                       <Col className="messageContainer" id={val.author === props.userName ? "You" : "Other"}>
-                          <Row className="messageIndividual">
+                          <Row className="messageIndividual flex-shrink-1">
                        {val.author === props.userName ? "You":val.author}: {val.message}
                            </Row>
                        </Col>
                      )})}
-                    </Row>
+                     </Row>
+                    </Scrollbars>
                 </Col>
             </Row>
             <Row className="controllers">
-                <Col sm='6'>
+                <Col sm='5'>
                     <input className="messageInputs"  type='text' placeholder='Message...' onChange={(e)=>{props.setMessage(e.target.value);}} />
                 </Col>
                 <Col sm='2'>
-                    <button onClick={props.sendMessage}>Send</button>
+                    <Button  onClick={props.sendMessage}>Send</Button >
                 </Col>
                 <Col sm='2'>
-                    <button onClick={props.saveConversation}>Save</button>
+                    <Button  onClick={props.saveConversation}>Save</Button >
                 </Col>
                 <Col sm='2'>
-                    <button onClick={props.openConversation}>Open</button>
+                    <Button  onClick={props.openConversation}>Open</Button >
                 </Col>
             </Row>
       </Container>
