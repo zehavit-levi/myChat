e1a1609eb488d491a73df5d64d96c3a56824e6fb
zehavit-levi <levi.zehavit@gmail.com>
2021-10-20 14:52:26 +0300

    add saveConversesion  worked, add bootstrap,add open-no working yet

--- client/src/pages/ChatRoom/ChatRoom.jsx
+++ client/src/pages/ChatRoom/ChatRoom.jsx
@@ -1,31 +1,44 @@
+import { Container, Row ,Col} from 'react-bootstrap';
 import './ChatRoom.css'
 function ChatRoom(props){
     return(
-        <div className="chatRoomContainer">
-            <div className="chatUsersList">
-                {props.usersList.map((val,key)=>{
-                    return(
-                        <div>{val}</div>
-                    )
-                })}
-            </div>
-            <div className="chatContainer">
-            <div className="messages">
-             {props.messageList.map((val,key)=>{
-              return (
-               <div className="messageContainer" id={val.author === props.userName ? "You" : "Other"}>
-                 <div className="messageIndividual">
-                   {val.author === props.userName ? "You":val.author}: {val.message}
-               </div>
-              </div>
-              )})}
-          </div>
-          <div className="messageInputs">
-              <input type='text' placeholder='Message...' onChange={(e)=>{props.setMessage(e.target.value);}} />
-              <button onClick={props.sendMessage}>Send</button>
-         </div>
-         </div>
-      </div>
+        <Container className='chatRoomContainer'>
+            <Row>
+                <Col className="chatUsersList">
+                    {props.usersList.map((val,key)=>{
+                        return(
+                         <div>{val}</div>
+                      )
+                  })}
+                </Col>
+                <Col className="chatContainer">
+                    <Row className="messages">
+                    {props.messageList.map((val,key)=>{
+                    return (
+                      <Col className="messageContainer" id={val.author === props.userName ? "You" : "Other"}>
+                          <Row className="messageIndividual">
+                       {val.author === props.userName ? "You":val.author}: {val.message}
+                           </Row>
+                       </Col>
+                     )})}
+                    </Row>
+                </Col>
+            </Row>
+            <Row className="controllers">
+                <Col sm='6'>
+                    <input className="messageInputs"  type='text' placeholder='Message...' onChange={(e)=>{props.setMessage(e.target.value);}} />
+                </Col>
+                <Col sm='2'>
+                    <button onClick={props.sendMessage}>Send</button>
+                </Col>
+                <Col sm='2'>
+                    <button onClick={props.saveConversation}>Save</button>
+                </Col>
+                <Col sm='2'>
+                    <button onClick={props.openConversation}>Open</button>
+                </Col>
+            </Row>
+      </Container>
     )
 }
 
